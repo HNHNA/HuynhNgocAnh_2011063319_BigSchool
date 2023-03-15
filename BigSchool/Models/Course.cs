c4254ad2095eb074dda3144384d9fc27043c00d8
huynh <huynh@LAPTOP-BHTC55BB>
2023-03-15 20:49:20 +0700

    Add project files.

---
+++ BigSchool/Models/Course.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Build.Framework.XamlTypes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public ApplicationUser Lecturer { get; set; }
+        //Bien DataAnnotations ,Required -> not null, 
+        [Required]
+        public string LecturerId { get; set; }
+        // StringLength -> toi da 255 ki tu
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+        public DateTime DateTime { get; set; }
+        public Category Category { get; set; }
+        [Required]
+        public byte CategoryId { get; set; }
+    }
+}
